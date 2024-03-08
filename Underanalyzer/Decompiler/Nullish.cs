4e8684b40db6bab669832dd756716dacf647f93a
colinator27 <17358554+colinator27@users.noreply.github.com>
2024-03-08 16:56:24 -0500

    Initial work on nullish operator and tests

---
+++ Underanalyzer/Decompiler/Nullish.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using static Underanalyzer.Decompiler.ShortCircuit;
+
+namespace Underanalyzer.Decompiler;
+
+public class Nullish : IControlFlowNode
+{
+    public enum NullishType
+    {
+        Expression,
+        Assignment
+    }
+
+    public int StartAddress { get; private set; }
+
+    public int EndAddress { get; private set; }
+
+    public List<IControlFlowNode> Predecessors { get; } = new();
+
+    public List<IControlFlowNode> Successors { get; } = new();
+
+    public IControlFlowNode Parent { get; set; } = null;
+
+    public List<IControlFlowNode> Children { get; } = [null];
+
+    public bool Unreachable { get; set; } = false;
+
+    public NullishType NullishKind { get; }
+
+    /// <summary>
+    /// The node that gets executed if the predecessor has a nullish value on the top of the stack after it.
+    /// </summary>
+    /// <remarks>
+    /// Upon being processed, this has its predecessors disconnected. 
+    /// All paths exiting from it are also isolated from the external graph.
+    /// </remarks>
+    public IControlFlowNode IfNullish { get => Children[0]; private set => Children[0] = value; }
+
+    public Nullish(int startAddress, int endAddress, NullishType nullishKind, IControlFlowNode ifNullishNode)
+    {
+        StartAddress = startAddress;
+        EndAddress = endAddress;
+        NullishKind = nullishKind;
+        IfNullish = ifNullishNode;
+    }
+
+    /// <summary>
+    /// Finds all nullish operations present in a list of blocks, and updates the control flow graph accordingly.
+    /// </summary>
+    public static List<Nullish> FindNullish(List<Block> blocks)
+    {
+        List<Nullish> res = new();
+
+        foreach (var block in blocks)
+        {
+            // Search for pattern
+            if (block.Instructions is 
+                [.., 
+                    { Kind: IGMInstruction.Opcode.Extended, ExtKind: IGMInstruction.ExtendedOpcode.IsNullishValue },
+                    { Kind: IGMInstruction.Opcode.BranchFalse }
+                ])
+            {
+                Block ifNullishBlock = block.Successors[0] as Block;
+                Block afterBlock = block.Successors[1] as Block;
+
+                // Determine nullish type by using the block "after"
+                NullishType nullishKind = NullishType.Expression;
+                if (afterBlock.Instructions is [{ Kind: IGMInstruction.Opcode.PopDelete }, ..])
+                    nullishKind = NullishType.Assignment;
+
+                Nullish n = new(block.EndAddress, afterBlock.StartAddress, nullishKind, ifNullishBlock);
+                res.Add(n);
+
+                // Remove instructions from this block
+                block.Instructions.RemoveRange(block.Instructions.Count - 2, 2);
+
+                // Remove pop instruction from "if nullish" block
+                ifNullishBlock.Instructions.RemoveAt(0);
+
+                Block endOfNullishBlock = null;
+                if (nullishKind == NullishType.Assignment)
+                {
+                    // Remove pop instruction from "after" block
+                    afterBlock.Instructions.RemoveAt(0);
+
+                    // Our "end of nullish" block is always before the "after" block.
+                    // Remove its branch instruction.
+                    endOfNullishBlock = blocks[afterBlock.BlockIndex - 1];
+                    endOfNullishBlock.Instructions.RemoveAt(endOfNullishBlock.Instructions.Count - 1);
+                }
+
+                // Disconnect sections of graph
+                IControlFlowNode.DisconnectPredecessor(ifNullishBlock, 0);
+                if (nullishKind == NullishType.Expression)
+                    IControlFlowNode.DisconnectPredecessor(afterBlock, 1);
+                IControlFlowNode.DisconnectPredecessor(afterBlock, 0);
+                if (endOfNullishBlock is not null)
+                    IControlFlowNode.DisconnectSuccessor(endOfNullishBlock, 0);
+
+                // Insert new node into graph
+                block.Successors.Add(n);
+                n.Predecessors.Add(block);
+                n.Successors.Add(afterBlock);
+                afterBlock.Predecessors.Add(n);
+            }
+        }
+
+        return res;
+    }
+}
---
+++ UnderanalyzerTest/Nullish.FindNullish.cs
@@ -0,0 +1,212 @@
+﻿using Underanalyzer;
+using Underanalyzer.Decompiler;
+using Underanalyzer.Mock;
+
+namespace UnderanalyzerTest;
+
+public class Nullish_FindNullish
+{
+    [Fact]
+    public void TestBasicExpression()
+    {
+        GMCode code = TestUtil.GetCode(
+            """
+            :[0]
+            push.v self.a
+            isnullish.e
+            bf [2]
+
+            :[1]
+            popz.v
+            push.v self.b
+
+            :[2]
+            """
+        );
+        List<Block> blocks = Block.FindBlocks(code);
+        List<Fragment> fragments = Fragment.FindFragments(code, blocks);
+        List<Nullish> nulls = Nullish.FindNullish(blocks);
+
+        Assert.Single(nulls);
+        Assert.Equal(Nullish.NullishType.Expression, nulls[0].NullishKind);
+        Assert.Equal([blocks[0]], nulls[0].Predecessors);
+        Assert.Equal([blocks[2]], nulls[0].Successors);
+        Assert.Equal([nulls[0]], blocks[0].Successors);
+        Assert.Equal([nulls[0]], blocks[2].Predecessors);
+        Assert.Equal(blocks[1], nulls[0].IfNullish);
+        Assert.Empty(blocks[1].Predecessors);
+        Assert.Empty(blocks[1].Successors);
+        Assert.True(blocks[0].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+        Assert.True(blocks[1].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+
+        TestUtil.VerifyFlowDirections(blocks);
+        TestUtil.VerifyFlowDirections(fragments);
+        TestUtil.VerifyFlowDirections(nulls);
+    }
+
+    [Fact]
+    public void TestBasicAssignment()
+    {
+        GMCode code = TestUtil.GetCode(
+            """
+            :[0]
+            push.v self.a
+            isnullish.e
+            bf [2]
+
+            :[1]
+            popz.v
+            push.v self.b
+            pop.v.v self.a
+            b [3]
+
+            :[2]
+            popz.v
+
+            :[3]
+            """
+        );
+        List<Block> blocks = Block.FindBlocks(code);
+        List<Fragment> fragments = Fragment.FindFragments(code, blocks);
+        List<Nullish> nulls = Nullish.FindNullish(blocks);
+
+        Assert.Single(nulls);
+        Assert.Equal(Nullish.NullishType.Assignment, nulls[0].NullishKind);
+        Assert.Equal([blocks[0]], nulls[0].Predecessors);
+        Assert.Equal([blocks[2]], nulls[0].Successors);
+        Assert.Equal([nulls[0]], blocks[0].Successors);
+        Assert.Equal([nulls[0]], blocks[2].Predecessors);
+        Assert.Equal(blocks[1], nulls[0].IfNullish);
+        Assert.Empty(blocks[1].Predecessors);
+        Assert.Empty(blocks[1].Successors);
+        Assert.True(blocks[0].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+        Assert.True(blocks[1].Instructions is [{ Kind: IGMInstruction.Opcode.Push }, { Kind: IGMInstruction.Opcode.Pop }]);
+        Assert.True(blocks[2].Instructions is []);
+        Assert.Equal([blocks[3]], blocks[2].Successors);
+
+        TestUtil.VerifyFlowDirections(blocks);
+        TestUtil.VerifyFlowDirections(fragments);
+        TestUtil.VerifyFlowDirections(nulls);
+    }
+
+    [Fact]
+    public void TestChainExpression()
+    {
+        GMCode code = TestUtil.GetCode(
+            """
+            :[0]
+            push.v self.a
+            isnullish.e
+            bf [2]
+
+            :[1]
+            popz.v
+            push.v self.b
+
+            :[2]
+            isnullish.e
+            bf [4]
+
+            :[3]
+            popz.v
+            push.v self.c
+
+            :[4]
+            """
+        );
+        List<Block> blocks = Block.FindBlocks(code);
+        List<Fragment> fragments = Fragment.FindFragments(code, blocks);
+        List<Nullish> nulls = Nullish.FindNullish(blocks);
+
+        Assert.Equal(2, nulls.Count);
+        Assert.Equal(Nullish.NullishType.Expression, nulls[0].NullishKind);
+        Assert.Equal([blocks[0]], nulls[0].Predecessors);
+        Assert.Equal([blocks[2]], nulls[0].Successors);
+        Assert.Equal([nulls[0]], blocks[0].Successors);
+        Assert.Equal([nulls[0]], blocks[2].Predecessors);
+        Assert.Equal(blocks[1], nulls[0].IfNullish);
+        Assert.Empty(blocks[1].Predecessors);
+        Assert.Empty(blocks[1].Successors);
+        Assert.True(blocks[0].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+        Assert.True(blocks[1].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+
+        Assert.Equal(Nullish.NullishType.Expression, nulls[1].NullishKind);
+        Assert.Equal([blocks[2]], nulls[1].Predecessors);
+        Assert.Equal([blocks[4]], nulls[1].Successors);
+        Assert.Equal([nulls[1]], blocks[2].Successors);
+        Assert.Equal([nulls[1]], blocks[4].Predecessors);
+        Assert.Equal(blocks[3], nulls[1].IfNullish);
+        Assert.Empty(blocks[3].Predecessors);
+        Assert.Empty(blocks[3].Successors);
+        Assert.Equal([nulls[1]], blocks[2].Successors);
+        Assert.True(blocks[2].Instructions is []);
+        Assert.True(blocks[3].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+
+        TestUtil.VerifyFlowDirections(blocks);
+        TestUtil.VerifyFlowDirections(fragments);
+        TestUtil.VerifyFlowDirections(nulls);
+    }
+
+    [Fact]
+    public void TestCombined()
+    {
+        GMCode code = TestUtil.GetCode(
+            """
+            :[0]
+            push.v self.a
+            isnullish.e
+            bf [4]
+
+            :[1]
+            popz.v
+            push.v self.b
+            isnullish.e
+            bf [3]
+
+            :[2]
+            popz.v
+            push.v self.c
+
+            :[3]
+            pop.v.v self.a
+            b [5]
+
+            :[4]
+            popz.v
+
+            :[5]
+            """
+        );
+        List<Block> blocks = Block.FindBlocks(code);
+        List<Fragment> fragments = Fragment.FindFragments(code, blocks);
+        List<Nullish> nulls = Nullish.FindNullish(blocks);
+
+        Assert.Equal(2, nulls.Count);
+        Assert.Equal(Nullish.NullishType.Assignment, nulls[0].NullishKind);
+        Assert.Equal([blocks[0]], nulls[0].Predecessors);
+        Assert.Equal([blocks[4]], nulls[0].Successors);
+        Assert.Equal([nulls[0]], blocks[0].Successors);
+        Assert.Equal([nulls[0]], blocks[4].Predecessors);
+        Assert.Equal(blocks[1], nulls[0].IfNullish);
+        Assert.Empty(blocks[1].Predecessors);
+        Assert.Empty(blocks[3].Successors);
+        Assert.True(blocks[0].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+        Assert.True(blocks[1].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+        Assert.True(blocks[4].Instructions is []);
+
+        Assert.Equal(Nullish.NullishType.Expression, nulls[1].NullishKind);
+        Assert.Equal([blocks[1]], nulls[1].Predecessors);
+        Assert.Equal([blocks[3]], nulls[1].Successors);
+        Assert.Equal([nulls[1]], blocks[1].Successors);
+        Assert.Equal([nulls[1]], blocks[3].Predecessors);
+        Assert.Equal(blocks[2], nulls[1].IfNullish);
+        Assert.Empty(blocks[2].Predecessors);
+        Assert.Empty(blocks[2].Successors);
+        Assert.True(blocks[2].Instructions is [{ Kind: IGMInstruction.Opcode.Push }]);
+        Assert.True(blocks[3].Instructions is [{ Kind: IGMInstruction.Opcode.Pop }]);
+
+        TestUtil.VerifyFlowDirections(blocks);
+        TestUtil.VerifyFlowDirections(fragments);
+        TestUtil.VerifyFlowDirections(nulls);
+    }
+}
