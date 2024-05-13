59542eb2585c610bbf446e2b3fd2ec529e9927c3
colinator27 <17358554+colinator27@users.noreply.github.com>
2024-05-13 15:48:44 -0400

    Add GlobalFunctions to mock

--- Underanalyzer/Mock/GameContextMock.cs
+++ Underanalyzer/Mock/GameContextMock.cs
@@ -1,9 +1,12 @@
-﻿namespace Underanalyzer.Mock;
+﻿using Underanalyzer.Decompiler;
+
+namespace Underanalyzer.Mock;
 
 public class GameContextMock : IGameContext
 {
     public bool UsingGMS2OrLater { get; set; } = true;
     public bool UsingGMLv2 { get; set; } = true;
     public bool Bytecode14OrLower { get; set; } = false;
+    public IGlobalFunctions GlobalFunctions { get; } = new GlobalFunctions();
     public string GetAssetName(int assetIndex, AssetType assetType) => null;
 }
